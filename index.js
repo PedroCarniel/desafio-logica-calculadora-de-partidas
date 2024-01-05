1b88df395ca37539f6853e339df194bdfcc14714
PedroCarniel <pedrohcastamancarniel@gmail.com>
2024-01-05 01:03:01 -0300

    Desafio resolvido

--- index.js
+++ index.js
@@ -25,4 +25,39 @@ Se vitórias for maior ou igual a 101 = Imortal
 Ao final deve se exibir uma mensagem:
 "O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"*/
 
+function calcularNivel(saldoVitorias){
+    let nivel;
 
+    if(saldoVitorias < 10){
+        nivel = "Ferro";
+    }
+    else if(saldoVitorias <= 20){
+        nivel = "Bronze";
+    }
+    else if(saldoVitorias <= 50){
+        nivel = "Prata";
+    }
+    else if(saldoVitorias <= 80){
+        nivel = "Ouro";
+    }
+    else if(saldoVitorias <= 90){
+        nivel = "Diamante";
+    }
+    else if(saldoVitorias < 100){
+        nivel = "Lendário";
+    }
+    else {
+        nivel = "Imortal";
+    }
+
+    return nivel;
+}
+
+// Teste da função
+let vitorias = 120;
+let derrotas = 13;
+
+let saldoVitorias = vitorias - derrotas;
+let nivelHeroi = calcularNivel(saldoVitorias);
+
+console.log(`O Herói tem um saldo de ${saldoVitorias} está no nível de ${nivelHeroi}`);
